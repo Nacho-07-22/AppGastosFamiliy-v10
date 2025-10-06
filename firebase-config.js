56850170a6c016389ecfab532b679c0fe7f927d6
Nacho-07-22 <xnachoxd22@gmail.com>
2025-10-05 20:55:32 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCL6PzooGnKGHv6rBFsUM2tvHEgA0qnCHQ",
+  authDomain: "app-gastosfamily.firebaseapp.com",
+  projectId: "app-gastosfamily",
+  storageBucket: "app-gastosfamily.firebasestorage.app",
+  messagingSenderId: "289077365892",
+  appId: "1:289077365892:web:3e403636333c3cd64a46d1",
+  measurementId: "G-FX4XJTVLXZ"
+};
+
+console.log("✅ Firebase config cargado correctamente");
